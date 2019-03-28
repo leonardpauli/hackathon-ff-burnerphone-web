5efddd82b898f1dfd0dd0d12cea1c91e3a96dcf3
Leonard Pauli <leonardpauli@me.com>
2019-03-28 19:42:58 +0100

    refactored config

---
+++ src/config/index.js
@@ -0,0 +1,54 @@
+// config
+
+const canonical = 'https://site.se'
+const siteimage = canonical+'/img/social/social-banner@1x.png'
+const title = '...'
+const desc = '...'
+
+const metaInfo = {
+	title: '...',
+	titleTemplate: '... - %s',
+	htmlAttrs: {
+		lang: 'en',
+	},
+	meta: [
+		// see https://alligator.io/vuejs/vue-seo-tips/
+
+		// {name: 'viewport'. content: 'width=device-width, initial-scale=1'},
+
+		{rel: 'canonical', href: canonical},
+
+		{name: 'description', content: desc},
+
+		// OpenGraph data (Most widely used)
+		{property: 'og:title', content: title},
+		{property: 'og:site_name', content: 'CodeVillage'},
+		// The list of types is available here: http://ogp.me/#types
+		{property: 'og:type', content: 'website'},
+		// Should the the same as your canonical link, see below.
+		{property: 'og:url', content: canonical},
+		{property: 'og:image', content: siteimage},
+		// Often the same as your meta description, but not always.
+		{property: 'og:description', content: desc},
+
+		// Twitter card
+		{name: 'twitter:card', content: 'summary'},
+		{name: 'twitter:site', content: canonical},
+		{name: 'twitter:title', content: title},
+		{name: 'twitter:description', content: desc},
+		// Your twitter handle, if you have one.
+		{name: 'twitter:creator', content: '@leonardpauli'},
+		{name: 'twitter:image:src', content: siteimage},
+
+		// Google / Schema.org markup:
+		{itemprop: 'name', content: title},
+		{itemprop: 'description', content: desc},
+		{itemprop: 'image', content: siteimage},
+	],
+}
+
+const config = {
+	metaInfo,
+}
+
+export default config
